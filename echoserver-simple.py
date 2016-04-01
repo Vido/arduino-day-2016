2da0b99ef68e591eb04fc835539d3229bc6a27ac
Lucas Vido <vidov8@gmail.com>
2016-04-01 13:50:36 -0300

    Update echoserver-simple.py

--- echoserver-simple.py
+++ echoserver-simple.py
@@ -10,13 +10,17 @@ host = ''
 port = 8266
 backlog = 5
 size = 1024
+
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((host,port))
 s.listen(backlog)
-while 1:
+
+while True:
     client, address = s.accept()
     data = client.recv(size)
+    
     if data:
-	print data
+        print data
         client.send(data)
+        
     client.close()
